dcbd001f76ba274a4a18dd754a45b5bf1766e4de
giuem <giuemcom@gmail.com>
2020-04-28 01:22:39 +0800

    feat: json schema

--- src/lib/monaco.ts
+++ src/lib/monaco.ts
@@ -1,10 +1,24 @@
 import { monaco as m } from "@monaco-editor/react";
 import * as Monaco from "monaco-editor";
 
+function onload(monaco: typeof Monaco) {
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    schemas: [
+      {
+        uri: "https://schemastore.azurewebsites.net/schemas/json/package.json",
+        fileMatch: ["package.json"]
+      }
+    ],
+    validate: true,
+    enableSchemaRequest: true
+  });
+}
+
 export let monaco: typeof Monaco;
 
 export const loadMonaco = () => {
   return m.init().then(m => {
+    onload(m);
     return (monaco = m);
   });
 };
