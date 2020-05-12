3f2be980b320c6f7e895decebb21c0170bb70363
giuem <giuemcom@gmail.com>
2020-05-12 22:19:26 +0800

    babel

--- src/lib/monaco.ts
+++ src/lib/monaco.ts
@@ -29,6 +29,40 @@ function onload() {
 
   listenPackageChange();
 
+  const compilerDefaults = {
+    jsxFactory: "React.createElement",
+    reactNamespace: "React",
+    jsx: monaco.languages.typescript.JsxEmit.React,
+    target: monaco.languages.typescript.ScriptTarget.ES2016,
+    allowNonTsExtensions: true,
+    moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+    experimentalDecorators: true,
+    noEmit: true,
+    allowJs: true,
+    typeRoots: ["node_modules/@types"]
+  };
+
+  monaco.languages.typescript.typescriptDefaults.setMaximumWorkerIdleTime(-1);
+  monaco.languages.typescript.javascriptDefaults.setMaximumWorkerIdleTime(-1);
+  monaco.languages.typescript.typescriptDefaults.setCompilerOptions(
+    compilerDefaults
+  );
+  monaco.languages.typescript.javascriptDefaults.setCompilerOptions(
+    compilerDefaults
+  );
+
+  monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+  monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+
+  monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: false
+  });
+  monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: false
+  });
+
   emmetHTML(monaco);
   emmetCSS(monaco);
 }
--- sw/process/js.js
+++ sw/process/js.js
@@ -2,6 +2,17 @@ import Babel from "@babel/standalone";
 
 export const processJS = content => {
   return Babel.transform(content, {
-    presets: ["env", "react"]
+    presets: [
+      [
+        "env",
+        {
+          modules: false,
+          targets: {
+            esmodules: true
+          }
+        }
+      ],
+      "react"
+    ]
   }).code;
 };
